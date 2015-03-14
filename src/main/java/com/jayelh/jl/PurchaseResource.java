e83c98ac87b38620e3cd74fae84bb672b2f9830b
Johan Lekberg <johanlekberg@Johans-MacBook-Pro-2.local>
2015-03-14 21:57:23 +0100

    Refactoring, adding .gitignore

--- src/main/java/com/jayelh/jl/PurchaseResource.java
+++ src/main/java/com/jayelh/jl/PurchaseResource.java
@@ -6,7 +6,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/")
-public class PurchaseService {
+public class PurchaseResource {
 
     @Context
     private PurchaseManager purchaseManager;
